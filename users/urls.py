c01930c05860d8afe363f2bd66abf451217bacfb
itzchav <itzchav3001@gmail.com>
2025-05-29 17:45:45 -0600

    Corrige inicio sesion 3

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,13 @@
 from django.urls import include, path
 from .views import RegisterUser, login_view, register_view, index, profile_view,user_profile
+from .views import current_user_profile
 
 urlpatterns = [
     path('', index, name='index'),  # Página principal HTML
     path('register/', register_view, name='register'),  # Mostrar formulario HTML
     path('login/', login_view, name='login'),  # Mostrar formulario HTML
     path('profile/', profile_view, name='profile'),  # 👈 Nuevo endpoint seguro
-    path('me/', user_profile, name='user-profile'),
+    path('me/', current_user_profile),
     #path('api/users/', include('users.urls')),
     path('api/', include('tasks.urls')),  # <- agrega esta línea para incluir tasks
 
