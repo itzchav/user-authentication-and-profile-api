249815595f44c6ece3294f28899b187c1d873626
itzchav <itzchav3001@gmail.com>
2025-05-30 17:49:32 -0600

    Prueba ci/cd 2

--- backend/urls.py
+++ backend/urls.py
@@ -11,14 +11,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('users.urls')),  # ← Deja esto para rutas frontend como /login, /profile, etc.
-
-    path('users/', include('users.urls')),  # 👈 AGREGA ESTO para que /users/me/ funcione
-    path('api/users/', include('users.api_urls')),  # ← Solo si tienes vistas REST separadas
-
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
     path('api/', include('tasks.urls')),
 ]
 
