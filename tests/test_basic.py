a0816f3ba176e95cf6af5ec595193d7372cc2ff3
itzchav <itzchav3001@gmail.com>
2025-05-30 23:49:13 -0600

    Prueba ci/cd pytest basico con error 3

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,2 +1,7 @@
-def test_always_passes():
-    assert True
+import pytest
+from django.urls import reverse
+
+@pytest.mark.django_db
+def test_healthcheck(client):
+    response = client.get("/")
+    assert response.status_code == 200
